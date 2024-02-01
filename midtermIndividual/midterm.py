be38d0723b64d3684da0a60daa3fb74b7936ebd5
alywisher <alliewish101@gmail.com>
2024-02-01 08:58:19 -0500

    midterm work again

--- midtermIndividual/midterm.py
+++ midtermIndividual/midterm.py
@@ -2,9 +2,28 @@
 #1/31/2024
 #Fourth Wing Dragon Data Base -> Midterm
 
+#menu Function
+
+#variable names: dragonName, namePronounce, nickName, meaning, color, type, riderFname, riderLname
+#def menu():
+
+#---- print colums ----
+#print(f"{rec[0]:14}\t{rec[1]:16}\t{rec[2]:8}\t{rec[3]:14}\t{rec[4]:8}\t{rec[5]:14}\t{rec[6]:10}\t{rec[7]:14}")
 
 
-#---- Main Code ----
+#---- MAIN CODE ----
+dragonName = []
+namePronounce = []
+nickName = []
+meaning = []
+color = []
+dragonType = [] 
+riderFname = []
+riderLname = []
+#---- MAIN HEADER PRINT ----
+print("-----------------------------------------------------------------------------------")
+print("\t\t\tWelcome to the Basgiath War College Dragon Encyclopedia")
+print("-----------------------------------------------------------------------------------")
 
 import csv
 with open("midtermIndividual/dragon.csv") as csvFile:
@@ -13,8 +32,15 @@ with open("midtermIndividual/dragon.csv") as csvFile:
     file = csv.reader(csvFile)
 
     for rec in file:
-
-        print(f"{rec[0]:14}\t{rec[1]:16}\t{rec[2]:8}\t{rec[3]:14}\t{rec[4]:8}\t{rec[5]:14}\t{rec[6]:10}\t{rec[7]:14}")
+        dragonName.append(rec[0])
+        namePronounce.append(rec[1])
+        nickName.append(rec[2])
+        meaning.append(rec[3])
+        color.append(rec[4])
+        dragonType.append(rec[5])
+        riderFname.append(rec[6])
+        riderLname.append(rec[7])
+        
 
         #dragonName.append(rec[0])
 
