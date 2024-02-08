a73e428065df925cb82d886da44ceaba96983bb4
alywisher <alliewish101@gmail.com>
2024-02-08 11:01:54 -0500

    midterm done

--- midtermIndividual/midterm.py
+++ midtermIndividual/midterm.py
@@ -2,65 +2,103 @@
 #1/31/2024
 #Fourth Wing Dragon Data Base -> Midterm
 
-#menu Function
+#Variable Dicktionary:
+# dragonName[rec(0)], namePronounce[rec(1)], nickName[rec(2)], meaning[rec(3)], dragonColor[rec(4)], dragonType[rec(5)], riderFname[rec(6)], riderLname[rec(7)] 
 
-#variable names: dragonName, namePronounce, nickName, meaning, color, type, riderFname, riderLname
-#---- print colums ----
-#print(f"{rec[0]:14}\t{rec[1]:16}\t{rec[2]:8}\t{rec[3]:14}\t{rec[4]:8}\t{rec[5]:14}\t{rec[6]:10}\t{rec[7]:14}")
 #---- FUNCTION ----
 def menu():
 
-    print("\t\t\t| Dragon Encyclopedia Menu | ")
-    print("\t\t\t1. Search By Dragon Name") #Show all info on Dragon
-    print("\t\t\t2. Search By Rider Name") #show all info on dragon
-    print("\t\t\t3. List Dragon Names") #Show only dragon names
-    print("\t\t\t4. List Rider Names") #show only rider names
-    print("\t\t\t5. EXIT")
+    print("-------------------------------------------------------------------------------")
+    print("\t| Welcome to the Basgiath War College Dragon Encyclopedia |")
+    print("-------------------------------------------------------------------------------")
 
-    choice = int(input("Please Enter your Choice [1-5]: "))
-    while choice != 1 and choice != 2 and choice != 3 and choice != 4 and choice != 5:
-        print("**ERROR** Please only enter digits [1-5] you halfwit")
-        choice = int(input("Please Enter your Choice [1-5]: "))
-    return choice
+    print("\n\t\t\t| Dragon Encyclopedia Menu |\n")
+    print("\t\t\t1. Search By Dragon Name") #Show all info on Dragon
+    print("\t\t\t2. Search By Dragon Color") #Show all info on Dragon (via color search)
+    print("\t\t\t3. Search By Dragon Type") #Show all info on Dragon (via type search)
+    print("\t\t\t4. Search By Rider Name") #show all info on dragon
+    print("\t\t\t5. List Dragon Names") #Show only dragon names
+    print("\t\t\t6. List Rider Names") #show only rider names
+    print("\t\t\t7. EXIT\n")
+
+    choice = input("Please Enter your Choice [1-7]: ")
+    while choice != '1' and choice != '2' and choice != '3' and choice != '4' and choice != '5' and choice != '6' and choice != '7':
+        print("**ERROR** Please only enter digits [1-7]")
+        choice = input("Please Enter your Choice [1-7]: ")
+    return int(choice) #stops code from breaking when enter is hit multiple times 
 
 def dragonSearch(name):
      #searchTerm is local var name and only exist in this def block
     ids = search(name,dragonName)
-    id = int(ids[0])
-    print("---------------------------------------------------------------------------------------------------\n")
-    print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
-    print(f"{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {color[id]:8} {dragonType[id]:14} {riderFname[id]:10} {riderLname[id]:14}")
-    input("")
+    if ids == 0:
+        print("**INVALID CONGRATS**") #ensure error doesnt break 
+    else:
+        print("----------------------------------------------------------------------------------------------------------------\n")
+        print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
+        for id in ids:
+            print(f"\t{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {dragonColor[id]:8} {dragonType[id]:14} {riderFname[id]:14} {riderLname[id]:14}")
+        
+def dragonColorSearch(color): #add ability to find via color
+     #searchTerm is local var name and only exist in this def block
+    ids = search(color, dragonColor)
+    if ids == 0:
+        print("**INVALID CONGRATS**") #ensure error doesnt break 
+    else:
+        print("----------------------------------------------------------------------------------------------------------------\n")
+        print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
+        for id in ids:
+            print(f"{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {dragonColor[id]:8} {dragonType[id]:14} {riderFname[id]:14} {riderLname[id]:14}")
+
+def dragonTypeSearch(type): #add ability to find via type
+     #searchTerm is local var name and only exist in this def block
+    ids = search(type, dragonType)
+    if ids == 0:
+        print("**INVALID CONGRATS**") #ensure error doesnt break 
+    else:
+        print("----------------------------------------------------------------------------------------------------------------\n")
+        print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
+        for id in ids:
+            print(f"{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {dragonColor[id]:8} {dragonType[id]:14} {riderFname[id]:14} {riderLname[id]:14}")
 
 def riderSearch(name, list):
      #searchTerm is local var name and only exist in this def block
     ids = search(name,list)
-    print("---------------------------------------------------------------------------------------------------\n")
-    print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
-    for id in ids:
-        print(f"{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {color[id]:8} {dragonType[id]:14} {riderFname[id]:10} {riderLname[id]:14}")
-    input("") 
+    if ids == 0:
+        print("**INVALID CONGRATS**")
+    else:
+        print("----------------------------------------------------------------------------------------------------------------\n")
+        print(f"{'DRAGON NAME':14} {'PRONOUNCIATION':16} {'NICK NAME':10} {'MEANING':14} {'COLOR':8} {'TYPE':14} {'RIDER FNAME':14} {'RIDER LNAME':10}")
+        for id in ids:
+            print(f"{dragonName[id]:14} {namePronounce[id]:16} {nickName[id]:10} {meaning[id]:14} {dragonColor[id]:8} {dragonType[id]:14} {riderFname[id]:14} {riderLname[id]:14}")
+    
     
 def search(identifier, list):
     #SEQUENTIAL SEARCH - "in sequence" -> start @ the beggining (0) go to the end (len(listName))
     ids = []
     for i in range(0, len(list)):
         #look at eatch value in a list to find what your looking for
-        if list[i] == identifier:
+        if list[i] == identifier.capitalize(): #hopefully works to cap. first letter and stop error from running when using lowercase, it works
             ids.append(i)
-    return ids        
+    if ids == []:
+        return 0
+    return ids
+
+def clear():
+    system('cls')
 
 #---- MAIN CODE ----
 dragonName = []
 namePronounce = []
 nickName = []
 meaning = []
-color = []
+dragonColor = []
 dragonType = [] 
 riderFname = []
 riderLname = []
-#---- MAIN HEADER PRINT ----
+#---- IMPORT CSV ---- 
 import csv
+from os import system, name
+
 with open("midtermIndividual/dragon.csv") as csvFile:
     #Reade from File
     
@@ -71,25 +109,44 @@ with open("midtermIndividual/dragon.csv") as csvFile:
         namePronounce.append(rec[1])
         nickName.append(rec[2])
         meaning.append(rec[3])
-        color.append(rec[4])
+        dragonColor.append(rec[4])
         dragonType.append(rec[5])
         riderFname.append(rec[6])
         riderLname.append(rec[7])
       
 #---- Disconect from csv ----
-#header:
-print("-------------------------------------------------------------------------------")
-print("\t| Welcome to the Basgiath War College Dragon Encyclopedia |")
-print("-------------------------------------------------------------------------------")
+
+#menu has built in header so that the user always sees the name of the program when the menu is shown 
+#IMPORTANT(kinda) -> Violet should only show up once when asking for the list of riders but show up twice on the rest of the selections
 
 menuChoice = ""
-while menuChoice != 5: # 5 is exit
+while menuChoice != 7: # 7 is exit
     menuChoice = menu()
     if menuChoice == 1:
         name = input("Enter Dragon Name: ")
         dragonSearch(name)
+        
+        input("\nPress enter to continue...")
+        
+        clear() #Will clear all previous info then move on 
 
     elif menuChoice == 2:
+        color = input("Enter Dragon Color: ")
+        dragonColorSearch(color)
+        
+        input("\nPress enter to continue...")
+        
+        clear() 
+
+    elif menuChoice == 3:
+        type = input("Enter Dragon type: ")
+        dragonTypeSearch(type)
+        
+        input("\nPress enter to continue...")
+        
+        clear() 
+
+    elif menuChoice == 4:
         #name = input("Enter the name of the Rider: ")
         check = ""
         while check != "F" and check != "L":
@@ -97,32 +154,47 @@ while menuChoice != 5: # 5 is exit
         name = input("Input the name: ")
         if check == "F":
             riderSearch(name, riderFname)
+            
+            input("\nPress enter to continue...")
+            
+            clear()
         else:
             riderSearch(name, riderLname)
+            
+            input("\nPress enter to continue...")
+            
+            clear()
 
-    elif menuChoice == 3:
+    elif menuChoice == 5:
         print("---------------------------------------------------") # -- just for seperation 
-        print("---| Showing Dragon Names |---") # -- Dragon names are in rec[0]
+        print("\t  ---| Showing Dragon Names |---") # -- Dragon names are in rec[0]
         print("---------------------------------------------------")
         for name in dragonName:
-            print (name)
+            print (f"\t\t{name}")
+        
+        input("\nPress enter to continue...")
+        
+        clear()
+
+    elif menuChoice == 6:
         print("---------------------------------------------------")
-        input("Press enter to continue...")
-        print("---------------------------------------------------")
-    elif menuChoice == 4:
-        print("---------------------------------------------------")
-        print("---| Showing Rider Names |---") # -- Rider names are in rec[6] for first and rec[7] for last
+        print("\t  ---| Showing Rider Names |---") # -- Rider names are in rec[6] for first and rec[7] for last
         print("---------------------------------------------------")
         temp = [] # -- run in temp to stop Violrt form printing twice
         for i in range(0, len(riderFname)):
             name = f"{riderFname[i]} {riderLname[i]}"
             if(name not in temp):
-                print(f"{riderFname[i]} {riderLname[i]}")
+                print(f"\t\t{riderFname[i]} {riderLname[i]}")
                 temp.append(name)
-        print("---------------------------------------------------")
-        input("Press enter to continue...")
-        print("---------------------------------------------------")
+        
+        input("\nPress enter to continue...")
+        
+        clear()
+    else:
+        print("-------------------------------------------------------------------------------")
+        print("    | Thank you for useing the: Basgiath War College Dragon Encyclopedia |")
+        print("-------------------------------------------------------------------------------")
 
-    
 
 
+#***WARNING 6 HOURS WORTH OF MENTAL ANGUISH WENT INTO THIS PLZ BE NICE***
